1e6a36798eb41840b6f29a09f03a460750de998f
manuel lantieri <manuel.lantieri02@gmail.com>
2025-03-03 10:37:06 +0100

    project init

---
+++ webapp-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Il mio progetto</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
