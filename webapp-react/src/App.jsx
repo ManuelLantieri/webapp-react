fc64d933d1b670ac5b6e5c5dc4bf614f3b395b16
manuel lantieri <manuel.lantieri02@gmail.com>
2025-03-04 09:54:39 +0100

    starter components and pages

--- webapp-react/src/App.jsx
+++ webapp-react/src/App.jsx
@@ -1,19 +1,18 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index Component={HomePage}></Route>
-        <Route to="/movie/" Component={MoviePage}></Route>
+        <Route Component={DefaultLayout}>
+          <Route index path="/" Component={HomePage}></Route>
+          <Route path="/movies/:id" Component={MoviePage}></Route>
+          <Route path="*" Component={PageNotFound}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
